18271d08b06ef0ad0714e5a1d3e38cad538e69dd
MatteoCarbone17 <matteocarbone17@live.com>
2022-12-10 19:11:29 +0100

    completed  snack JS Simon

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
 
 
 const button = document.getElementById('play')
+console.log(button);
 
-if (counter >= 10) {
-    setTimeout(Myfunction(){
-    button.classList.add('bg-red');
-    }, 10000);
+
+let counter = 0; 
+let isGameOver = false ;
+
+button.innerHTML = `Click (${counter})` 
+
+button.addEventListener('click' , function(){
+
+if (counter < 10) {
+
+    counter++;
+
+    button.innerHTML = `Click (${counter})` 
+
+} else{
+    isGameOver = true;
     
+    button.classList.add('bg-danger');
+
+    setTimeout(() => {
+        
+        isGameOver = false; 
+
+        counter = 0; 
+
+        button.innerHTML = `Click (${counter})`
+    
+        button.classList.remove('bg-danger');
+    }, 700);
 }
+
+console.log(counter)
+
+})
